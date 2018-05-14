85954d858b809e2454b4f480cb8b90e2501c28bf
Seha Gulnar Dalay <sehagulnar@gmail.com>
2018-05-14 12:21:47 -0400

    nth Triangular Number

---
+++ triangularNumber.js
@@ -0,0 +1,9 @@
+'use strict';
+function calculateSumOf(num){
+    if( num === 1){
+        return num;
+    }else{
+        return num + calculateSumOf(num-1);
+    }
+}
+console.log(calculateSumOf(5));
