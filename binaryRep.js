751690a3547bb1bcb14ef5bc4e1a247462f87105
Seha Gulnar Dalay <sehagulnar@gmail.com>
2018-05-14 14:54:31 -0400

    factorial

--- binaryRep.js
+++ binaryRep.js
@@ -1,10 +1,23 @@
 'use strict';
-function findBinary(num){
-  if(num > 0) {
-    let newNum= Math.floor(num % 2);
-    return (findBinary(Math.floor(num / 2)) + newNum);
-  }else{
-    return '';
+const findBinary = (num) => {
+  if (num === 0) {
+    return '0';
   }
-}
+  const dividedNum = Math.floor(num / 2);
+  const remainder = num % 2;
+  if (dividedNum === 0) {
+    return `${remainder}`;
+  }
+  return findBinary(dividedNum) + remainder.toString();
+};
 console.log(findBinary(25));
+
+// function findBinary(num){
+//   if(num > 0) {
+//     let newNum= Math.floor(num % 2);
+//     return (findBinary(Math.floor(num / 2)) + newNum);
+//   }else{
+//     return '';
+//   }
+// }
+// console.log(findBinary(25));
