6243ae517da4fcfa1b38c476a4509e3262352da2
Seha Gulnar Dalay <sehagulnar@gmail.com>
2018-05-14 15:11:35 -0400

    fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,11 @@
+'use strict';
+function fibonacci(num){
+  if(num < 1){
+    return 0;
+  }if(num < 2 ){
+    return 1;
+  }else{
+    return fibonacci(num-1) + fibonacci(num-2);
+  }
+}
+console.log(fibonacci(9));
